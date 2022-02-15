d83a2e1aa169f945971f024cbbc57959eebbbba8
GriffinPitts <96311010+GriffinPitts@users.noreply.github.com>
2022-02-15 07:39:15 -0500

    Add files via upload

---
+++ webDev/script.js
@@ -0,0 +1,19 @@
+function changeFontRed() {
+    var x = document.getElementById("head");
+
+    x.style.color = "red"; 
+    //x.style["-webkit-text-stroke"] = "1px black";
+}
+
+function changeFontBlue() {
+    var x = document.getElementById("head");   
+
+    x.style.color = "blue"; 
+    //x.style["-webkit-text-stroke"] = "1px black";
+}
+function changeFontDefault() {
+    var x = document.getElementById("head");   
+    
+    x.style.color = "white"; 
+    //x.style.removeProperty("-webkit-text-stroke");
+}
