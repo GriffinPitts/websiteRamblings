eac7cb595f018043bd95221f4fbec885ae624ae6
GriffinPitts <96311010+GriffinPitts@users.noreply.github.com>
2022-02-17 01:59:08 -0500

    Spacings removed, JUnit test fixture added (kinda)

---
+++ webDev/IndexGenerator/test/IndexGeneratorTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class IndexGeneratorTest {
+
+    @Test
+    public void test() {
+        assertEquals("a", "a");
+    }
+}
