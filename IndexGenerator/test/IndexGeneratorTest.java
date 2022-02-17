51aa1a81069094137008c644d99c4b0bb3e20a95
GriffinPitts <96311010+GriffinPitts@users.noreply.github.com>
2022-02-17 02:31:57 -0500

    Messing with settings.json

--- IndexGenerator/test/IndexGeneratorTest.java
+++ IndexGenerator/test/IndexGeneratorTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class IndexGeneratorTest {
+public final class IndexGeneratorTest {
 
     @Test
     public void test() {
